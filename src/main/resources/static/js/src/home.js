37f6a0a18108693eab242e6b423f1d18c4110e24
Bo Yan <boyan.au@gmail.com>
2018-04-01 12:47:06 +1000

    copy example in.

--- src/main/resources/static/js/src/home.js
+++ src/main/resources/static/js/src/home.js
@@ -1,7 +1,13 @@
+// register modal component
+Vue.component('modal', {
+  template: '#modal-template'
+})
+
 var vm = new Vue({
 	el: '#app',
 	data: {
 		models:[],
+		showModal: false
 	},
 	mounted:function(){
         this.onLoad();
