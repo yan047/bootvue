5aad340e19de2fdbba163ce4a3e1e28385c94f7e
Bo Yan <boyan.au@gmail.com>
2018-04-01 13:20:44 +1000

    add 2nd dialog template into page.

--- src/main/resources/static/js/src/home.js
+++ src/main/resources/static/js/src/home.js
@@ -2,12 +2,17 @@
 Vue.component('modal1', {
   template: '#modal-template'
 })
+// register modal component
+Vue.component('modal2', {
+  template: '#modal-template2'
+})
 
 var vm = new Vue({
 	el: '#app',
 	data: {
 		models:[],
-		showModal1: false
+		showModal1: false,
+		showModal2: false
 	},
 	mounted:function(){
         this.onLoad();
